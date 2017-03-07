a30f2d617b8b95d9b4c2b3b05f0fc3065c401d4a
SahilDhawan <sahildhawan2011@gmail.com>
2017-03-07 19:49:37 +0530

    Added Firebase Auth

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
-
+import Firebase
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
     @IBOutlet weak var entryView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.entryView.layer.cornerRadius = 5
         self.entryView.clipsToBounds = true
         self.navigationController?.setNavigationBarHidden(true, animated: true)
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+
     }
     override var preferredStatusBarStyle: UIStatusBarStyle
     {
@@ -25,7 +35,28 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func registerPressed(_ sender: Any) {
+        
+        guard let email = emailTextField.text,  let password = passwordTextField.text else
+        {
+            print("Form Entries are not valid")
+            return
+        }
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user: FIRUser?, error) in
+//            if error != nil
+//            {
+//                print("Error in Authentication")
+//                return
+//            }
+        })
+    }
+}
+extension LoginViewController : UITextFieldDelegate
+{
+     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
