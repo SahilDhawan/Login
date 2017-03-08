69f02a1d63a059665453ebeebe0753deb203addc
SahilDhawan <sahildhawan2011@gmail.com>
2017-03-08 22:49:25 +0530

    Corrected UI

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 import Firebase
-class LoginViewController: UIViewController {
 
+
+class LoginViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
-    
-    
+    @IBOutlet weak var segmentController: UISegmentedControl!
+    @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var entryView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.entryView.layer.cornerRadius = 5
@@ -25,57 +26,75 @@ class LoginViewController: UIViewController {
         nameTextField.delegate = self
         emailTextField.delegate = self
         passwordTextField.delegate = self
-
     }
     override var preferredStatusBarStyle: UIStatusBarStyle
     {
         return .lightContent
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func registerPressed(_ sender: Any) {
+        
         
         guard let email = emailTextField.text,  let password = passwordTextField.text, let name = nameTextField.text else
         {
             print("Form Entries are not valid")
             return
         }
+        
         FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user: FIRUser?, error) in
             if error != nil
             {
-                self.showError(msg: "Password must be 6 digits or greater")
+                self.showAlert(msg: "Password must be of 6 digits or greater")
                 
+                return
+            }
+            guard let uid = user?.uid else
+            {
+                self.showAlert(msg: "Not a valid user id")
                 return
             }
             let ref = FIRDatabase.database().reference(fromURL: "https://login-8d277.firebaseio.com/")
             let values = ["name": name, "email":email]
-            ref.updateChildValues(values, withCompletionBlock: { (err, ref) in
+            let users = ref.child("users").child(uid)
+            users.updateChildValues(values, withCompletionBlock: { (err, ref) in
                 if err != nil
                 {
                     print(err)
                     return
                 }
-                self.showError(msg: "Login Successfull")
-                
+                self.showAlert(msg: name + " registered successfully!")
             })
         })
-        
     }
-    func showError(msg : String)
+    func showAlert(msg : String)
     {
         let alert = UIAlertController.init(title: "Login/Register", message: msg, preferredStyle: .alert)
         let alertAction = UIAlertAction.init(title: "Dismiss", style: .default, handler:nil)
         alert.addAction(alertAction)
         self.present(alert, animated: true, completion: nil)
     }
+    
+    @IBAction func segmentControllerPressed(_ sender: Any) {
+        
+        let value = segmentController.titleForSegment(at: segmentController.selectedSegmentIndex)
+        registerButton.setTitle(value, for: .normal)
+        entryView.translatesAutoresizingMaskIntoConstraints = false
+        let height:CGFloat = segmentController.selectedSegmentIndex == 0 ? 100.00 : 150.00
+        let bool = segmentController.selectedSegmentIndex == 0 ? true : false
+        self.nameTextField.isHidden = bool
+        
+        let heightContraint = entryView.heightAnchor.constraint(equalToConstant: height)
+        heightContraint.isActive = true
+        heightContraint.isActive = false
 
+
+
+//        self.showAlert(msg: "height changed \(height)")
+    }
 }
 extension LoginViewController : UITextFieldDelegate
 {
-     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
